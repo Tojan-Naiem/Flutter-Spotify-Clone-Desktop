b9372c16109352ce062b9aa56aa4f9c8ff98566a
Tojann204 <tojan050@gmail.com>
2024-05-03 17:29:45 +0300

    commit

--- lib/models/song.dart
+++ lib/models/song.dart
@@ -1,5 +1,109 @@
 class Song{
 
+String? title;
+String? album;
+String? artist;
+String? duration;
+
+Song({
+required this.title,
+required this.artist,
+required this.album,
+required this.duration,
+
+});
+
+static List songs=[
+
+ Song(
+ 
+    title: 'Snowman',
+    artist: 'WYS',
+    album: '1 Am. Study Session',
+    duration: '3:15',
+  ),
+  Song(
+
+    title: 'Healthy Distraction',
+    artist: 'less.people',
+    album: 'One Day It\'s Over',
+    duration: '2:18',
+  ),
+  Song(
+
+    title: 'Far Away',
+    artist: 'Mila Coolness',
+    album: 'Silent River',
+    duration: '2:39',
+  ),
+  Song(
+
+    title: 'Call You Soon',
+    artist: 'Louk, Glimlip',
+    album: 'Can We Talk',
+    duration: '2:35',
+  ),
+  Song(
+
+    title: 'Winter Sun',
+    artist: 'Bcalm, Banks',
+    album: 'Feelings',
+    duration: '2:15',
+  ),
+  Song(
+
+    title: 'Hope',
+    artist: 'No Spirit',
+    album: 'Memories We Made',
+    duration: '1:57',
+  ),
+  Song(
+ 
+    title: 'A Better Place',
+    artist: 'Project AER, cxit.',
+    album: 'Growth Patterns',
+    duration: '2:00',
+  ),
+  Song(
+
+    title: 'Misty Dawn',
+    artist: 'BluntOne',
+    album: 'Autumn in Budapest',
+    duration: '2:34',
+  ),
+  Song(
+
+    title: 'Hourglass',
+    artist: 'Thaehan',
+    album: 'Hourglass',
+    duration: '1:43',
+  ),
+  Song(
+
+    title: 'After Sunset',
+    artist: 'Project AER, WYS',
+    album: '3 Am. Study Session',
+    duration: '2:41',
+  ),
+  Song(
+
+    title: 'Child',
+    artist: 'Ambulo',
+    album: 'Polar',
+    duration: '2:12',
+  ),
+  Song(
+
+    title: 'Arizona Zero',
+    artist: 'WYS, Sweet Medicine',
+    album: 'Evermore',
+    duration: '2:31',
+  ),
+
+
+
+
+];
 
 
 
--- lib/widget/list.dart
+++ lib/widget/list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_clone_desktop/models/song.dart';
 
 class List extends StatefulWidget {
   const List({super.key});
@@ -6,21 +7,45 @@ class List extends StatefulWidget {
   @override
   State<List> createState() => _ListState();
 }
-
+// Color.fromRGBO(255, 255, 255, 0.5)
 class _ListState extends State<List> {
   @override
   Widget build(BuildContext context) {
     return DataTable(
+
       columns: [
-        DataColumn(label:const Text('TITLE',style: TextStyle(color: const Color.fromRGBO(255, 255, 255, 0.5),fontFamily:'Montserrat',fontSize: 12),),
+        DataColumn(label:const Text('TITLE',style: TextStyle(color:  Colors.white,fontFamily:'Montserrat',fontSize: 12),),
  ),
-    DataColumn(label:const Text('ARTIST',style: TextStyle(color: const Color.fromRGBO(255, 255, 255, 0.5),fontFamily:'Montserrat',fontSize: 12),),),
-      DataColumn(label:const Text('ALBUM',style: TextStyle(color: const Color.fromRGBO(255, 255, 255, 0.5),fontFamily:'Montserrat',fontSize: 12),),),
+    DataColumn(label:const Text('ARTIST',style: TextStyle(color: Colors.white,fontFamily:'Montserrat',fontSize: 12),),),
+      DataColumn(label:const Text('ALBUM',style: TextStyle(color: Colors.white,fontFamily:'Montserrat',fontSize: 12),),),
       DataColumn(label:const   Icon(Icons.access_time,color: Colors.white,)),
       
       ],
-      rows: [],
+      rows: [
+           for(int i=0;i<Song.songs.length;i++)
+           dataRow(Song.songs[i])
+
+
+      ],
    
+    );
+  }
+  DataRow dataRow(Song song){
+
+    return DataRow(
+      
+      cells:[
+        dataCell(song.title!),
+        dataCell(song.artist!),
+        dataCell(song.album!),
+        dataCell(song.duration!),
+      ] );
+  }
+  DataCell dataCell(String s){
+    return DataCell(
+
+     Text(s,style: TextStyle(color:  Colors.white,fontFamily:'Montserrat',fontSize: 12),)
+
     );
   }
 }
